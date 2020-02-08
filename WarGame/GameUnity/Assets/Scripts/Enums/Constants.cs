1f858133c282f41179cb28f7c8e75752441682f5
Jasper de Kroon <jasperdekroon2000@gmail.com>
2020-02-08 17:59:18 +0100

    Unity project
    - Opzet nieuw unity project voor betere shaders

---
+++ WarGame/GameUnity/Assets/Scripts/Enums/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Constants
+{
+	public class Tags
+	{
+		public const string UNTAGGED = "UNTAGGED";
+		public const string RESPAWN = "RESPAWN";
+		public const string FINISH = "FINISH";
+		public const string EDITORONLY = "EDITORONLY";
+		public const string MAINCAMERA = "MAINCAMERA";
+		public const string PLAYER = "PLAYER";
+		public const string GAMECONTROLLER = "GAMECONTROLLER";
+		public const string PLAYTILE = "PLAYTILE";
+	}
+
+	public class Layers
+	{
+
+	}
+}
+
